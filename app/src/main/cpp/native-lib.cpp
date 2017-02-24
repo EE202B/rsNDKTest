523dc8800c5cd90e535c31f736a0d1a5fa374557
coroner4817 <wang4817@umn.edu>
2017-02-24 01:30:06 -0800

    remove hard code stuff

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -5,6 +5,7 @@
 
 #include <cpp/RenderScript.h>
 
+
 #define  LOG_TAG    "native-lib-rs"
 #define  LOGI(...)  __android_log_print(ANDROID_LOG_INFO,LOG_TAG,__VA_ARGS__)
 #define  LOGE(...)  __android_log_print(ANDROID_LOG_ERROR,LOG_TAG,__VA_ARGS__)
